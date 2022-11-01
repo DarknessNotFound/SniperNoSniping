16f3f0d5559a4d33dfe2f60395a221db1c8983fc
DarknessNotFound <galbrightr@gmail.com>
2022-10-31 23:50:24 -0500

    version alpha 1.0

--- PROD/cogs/SniperTemp.py
+++ PROD/cogs/SniperTemp.py
@@ -10,10 +10,8 @@ class SnipeTemp_Commands(commands.Cog):
     # Commands
     @commands.command(name='snipe', help='Adds a snipe to the database')
     async def snipe(self, ctx, *args):
-        print("Snipe executed")
         #Guard clause against wrong channel.
         if accessible_channel(ctx) == False:
-            print("snipe -- wrong channel")
             await ctx.send("Please send commands in the Sniper bot channel!")
             return
 
@@ -25,13 +23,11 @@ class SnipeTemp_Commands(commands.Cog):
 
         #Get the time
         timestamp = datetime.now()
-
         #Add the sniped to the database and send confirmation message
         if(AddTempSnipe(sniper, sniped, timestamp) == True):
             await ctx.send(f"{sniper} sniped {sniped}  successfully at {timestamp}!")
         else:
             await ctx.send(f"{sniper} sniping {sniped} failed to add to database... please try again")
-        print("snipe finished execution")
 
     @commands.command(name='AllSnipes', help='Shows all of the snipes in the database')
     async def AllSnipes(self, ctx, *args):
@@ -39,7 +35,6 @@ class SnipeTemp_Commands(commands.Cog):
         #Guard clause against wrong channel.
         if accessible_channel(ctx) == False:
             await ctx.send("Please send commands in the Sniper bot channel!")
-            print("AllSnipes Bad Channel")
             return
 
         try:
@@ -59,7 +54,8 @@ class SnipeTemp_Commands(commands.Cog):
     @commands.command(name='SnipingSeason', help='Shows an announcement for the start of sniping season')
     async def SnipingSeason(self, ctx, *args):
         print("Started Sniping Season")
-        if ctx.author.name != 'GmanBeCrazy':
+        # if ctx.author.name != 'GmanBeCrazy':
+        if False:
             await ctx.send("This command is too powerful for you to use!")
             return
         embed=discord.Embed(
@@ -70,7 +66,7 @@ class SnipeTemp_Commands(commands.Cog):
         embed.add_field(name="Try outs begin now", value="You have from now through November to impress me and my panel of judges. Use the `>>snipe @Person` and post the picture to let my judges know when you get someone.")
         embed.add_field(name="Payment", value="I will give a prize to the one who has the most snipes and a compensation to the one who got sniped the most.")
         embed.add_field(name="Happy Hunting", value="Dora's next adventure will *clear* her head.", inline=False)
-        embed.set_footer(text="NOTE: if the person isn't in the discord but is a legal snipe, use their first name to help the moderators count snipes at the end of the season.")
+        embed.set_footer(text="NOTE: if the person isn't in the discord but is a legal snipe, use their first and last name to help the moderators count snipes at the end of the season.")
         await ctx.send(embed=embed)
 
 async def setup(client):
--- PROD/funcs.py
+++ PROD/funcs.py
@@ -2,12 +2,22 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-CHANNEL_IDS = [os.getenv('PROD_SNIPING_CHANNEL_ID'),
-              os.getenv('PROD_SNIPING_ADMIN_CHANNEL_ID'),
-              os.getenv('PROD_BOT_TESTING_CHANNEL_ID') ]
+CHANNEL_IDS = [str(os.getenv('PROD_SNIPING_CHANNEL_ID')),
+              str(os.getenv('PROD_SNIPING_ADMIN_CHANNEL_ID')),
+              str(os.getenv('PROD_BOT_TESTING_CHANNEL_ID')) ]
 
 def accessible_channel(ctx):
-    if ctx.channel.id in CHANNEL_ID:
-        return True
-    else:
+    return True
+    try:
+        print("Accessible channel called")
+        if (ctx.channel.id in CHANNEL_IDS):
+            print("Accessible channel returned true")
+            return True
+        else:
+            print("Accessible channel returned false")
+            return False
+    except Exception as ex:
+        print(f"funcs -- accessible_channel -- {ex}")
+
+    finally:
         return False
